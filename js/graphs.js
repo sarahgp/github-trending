956ea92025fe163e354ed0fb13d6c4621d85fbe7
Sarah GP <breakers@Breakerss-MacBook-Air.local>
2013-09-09 07:13:44 -0700

    Column scaffolding, axes drawing, column not

--- js/graphs.js
+++ js/graphs.js
@@ -1,8 +1,8 @@
-function draw() {
+function drawSpiral() {
 
-  var h = 400,
-    w = 400,
-    r = Math.min(w, h) / 1.9,
+  var height = 400,
+    width = 400,
+    r = Math.min(width, height) / 1.9,
     s = .09;
 
   var fill = ["#047f00", "#52ff4c", "#08ff00", "#2f912c", "#07cc00"];
@@ -14,10 +14,10 @@ function draw() {
     .outerRadius(function(d) { return (d.index + s) * r; });
 
   var svg = d3.select(".one").append("svg")
-    .attr("width", w)
-    .attr("height", h)
+    .attr("width", width)
+    .attr("height", height)
   .append("g")
-    .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 
   d3.json("manual-data.json", function(error, data){
@@ -43,7 +43,7 @@ function draw() {
         contributors = d.contributors;
 
       return [
-        {value: watchers, index: .5, text: keys[0]},
+        {value: watchers, index: .4, text: keys[0]},
         // {value: stargazers, index: .4, text: keys[1]},
         {value: forks, index: .3, text: keys[2]},
         {value: pulls, index: .2, text: keys[3]},
@@ -60,7 +60,6 @@ function draw() {
       .enter().append("g");
 
     g.append("path")
-            // .style("fill", "#52ff4c")
         .style("fill", function(d, i) { return fill[i]; })
         .attr("d", arc);
 
@@ -68,8 +67,56 @@ function draw() {
         .attr("text-anchor", "middle")
         .attr("dy", "1em")
         .text(function(d,i) { return d.text; });
+  })
 
+}
 
+function drawColumn() {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 400 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1, 1);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select(".one-below").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("manual-data.json", function(error, data){
+
+    x.domain(data.today.one.stargazers);
+    y.domain([0, d3.max(data, function(d){return d.today.one.stargazers})]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+    svg.selectAll()
+        .data(data)
+      .enter().append("rect")
+        .attr("x", "50px")
+        .attr("width", "20px")
+        .attr("y", function(d) { return y(data.today.one.stargazers); })
+        .attr("height", function(d) { return height - y(data.today.one.stargazers); });
 
   })
 
@@ -77,4 +124,10 @@ function draw() {
 
 }
 
-$(document).ready(draw);
+$(document).ready(
+  function(){
+    drawSpiral()
+    drawColumn();
+  }
+
+);
